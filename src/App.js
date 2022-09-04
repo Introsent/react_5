a2ac7f243ede60ad64fdd49b1b19623c1ce3731a
Introsent <72797970+Introsent@users.noreply.github.com>
2022-09-04 14:26:46 +0300

    Дз за 5 урок ReactJS

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Count from "./count";
+
+
+function App() {
+  return (
+    <div>
+      <Count />
+    </div>
+  );
+}
+
+export default App;
